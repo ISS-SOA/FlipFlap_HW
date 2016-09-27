61a1f490fd93bc54b6e10a628f3684d43b7d45ed
Soumya Ray <soumya.ray@gmail.com>
2016-09-27 15:01:48 +0800

    Update tests to check for output files

--- flip_flap_spec.rb
+++ flip_flap_spec.rb
@@ -1,16 +1,20 @@
 require 'minitest/autorun'
+require 'minitest/rg'
+require 'open3'
 
-describe 'Serializer specs' do
+describe 'FlipFlap specs' do
   it 'should convert yml to tsv' do
     `ruby yml_to_tsv.rb data/survey.yml data/test.tsv`
-    diff = `diff data/survey.tsv data/test.tsv`
-    diff.must_equal ''
+    File.file?('data/test.tsv').must_equal true
+    diff = FileUtils.compare_file('data/survey.tsv', 'data/test.tsv')
+    diff.must_equal true
   end
 
   it 'should convert tsv to yml' do
     `ruby tsv_to_yml.rb data/survey.tsv data/test.yml`
-    diff = `diff data/survey.yml data/test.yml`
-    diff.must_equal ''
+    File.file?('data/test.yml').must_equal true
+    diff = FileUtils.compare_file('data/survey.yml', 'data/test.yml')
+    diff.must_equal true
   end
 
   after do
