b59e07597c687f55afec1e00851b60c621b85d45
Soumya Ray <soumya.ray@gmail.com>
2017-09-26 09:22:18 +0800

    Update mintest specs to new notation

--- flip_flap_spec.rb
+++ flip_flap_spec.rb
@@ -8,37 +8,37 @@ TSV_DATA = File.read('data/survey.tsv')
 
 describe 'FlipFlap specs' do
   it 'supports tsv inputs' do
-    FlipFlap.input_formats.must_include 'tsv'
+    _(FlipFlap.input_formats).must_include 'tsv'
   end
 
   it 'supports yaml inputs' do
-    FlipFlap.input_formats.must_include 'yaml'
+    _(FlipFlap.input_formats).must_include 'yaml'
   end
 
   it 'should convert yml to tsv' do
     flipper = FlipFlap.new
     flipper.take_yaml(YML_DATA)
-    flipper.to_tsv.must_equal TSV_DATA
+    _(flipper.to_tsv).must_equal TSV_DATA
   end
 
   it 'should convert tsv to yml' do
     flipper = FlipFlap.new
     flipper.take_tsv(TSV_DATA)
-    flipper.to_yaml.must_equal YML_DATA
+    _(flipper.to_yaml).must_equal YML_DATA
   end
 
   it 'should store Yaml in data we can retrieve' do
     flipper = FlipFlap.new
-    flipper.data.must_be_nil
+    _(flipper.data).must_be_nil
     flipper.take_yaml(YML_DATA)
-    flipper.data.wont_be_nil
+    _(flipper.data).wont_be_nil
   end
 
   it 'should store TSV in data we can retrieve' do
     flipper = FlipFlap.new
-    flipper.data.must_be_nil
+    _(flipper.data).must_be_nil
     flipper.take_tsv(TSV_DATA)
-    flipper.data.wont_be_nil
+    _(flipper.data).wont_be_nil
   end
 end
 
@@ -47,14 +47,14 @@ describe 'application specs' do
     `ruby yml_to_tsv.rb data/survey.yml data/test.tsv`
     File.file?('data/test.tsv').must_equal true
     diff = FileUtils.compare_file('data/survey.tsv', 'data/test.tsv')
-    diff.must_equal true
+    _(diff).must_equal true
   end
 
   it 'should convert tsv to yml from command line' do
     `ruby tsv_to_yml.rb data/survey.tsv data/test.yml`
     File.file?('data/test.yml').must_equal true
     diff = FileUtils.compare_file('data/survey.yml', 'data/test.yml')
-    diff.must_equal true
+    _(diff).must_equal true
   end
 
   after do
